714a506df685e5b3bc4c86b8ef01ffaf8b888191
Calvin <Calvin@MacBook-Pro-2.local>
2025-05-26 11:14:08 +0700

    Add Sentiment Analysis and Text Dataviz

---
+++ Sentiment_analysis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from transformers import pipeline
+
+class Sentiment():
+    def __init__(self):
+        self.filepath = ''
+        self.model = 'w11wo/indonesian-roberta-base-sentiment-classifier'
+        self.classifier = pipeline("sentiment-analysis", model=self.model)
+
+    def getSentimentLabel(self, text):
+        return self.classifier(text)[0]['label']
+    
+    def getSentimentScore(self, text):
+        return self.classifier(text)[0]['score']
